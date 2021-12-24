5d2f9d1cf49a56ea3eb8f85d66655ac9ca1b6f8c
IkoAfianando <ikoafianando123@gmail.com>
2021-12-24 14:18:16 +0700

    Push

--- standart-library.mjs
+++ standart-library.mjs
@@ -2,4 +2,7 @@ import os from "os";
 
 console.info(os.platform());
 console.table(os.cpus());
-
+console.info(os.networkInterfaces());
+console.info(os.version());
+console.info(os.release());
+console.info(os.totalmem());
