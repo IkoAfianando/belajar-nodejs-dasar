ebead73fd06fac23ff67f3535f2e87d4fd2da5b1
IkoAfianando <ikoafianando123@gmail.com>
2021-12-25 11:16:21 +0700

    Push

---
+++ console.mjs
@@ -0,0 +1,18 @@
+import {Console} from "console";
+import fs from "fs";
+
+const file = fs.createWriteStream("application.log");
+
+const log = new Console({
+    stdout: file,
+    stderr: file,
+});
+
+log.info("Hello World");
+log.error("Hello World");
+
+const person = {
+    firstName: "Iko",
+    lastName: "Afianando"
+}
+log.table(person);
