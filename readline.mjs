a35aa5e137f3b88b9a8146a9536f7cb605d0cf44
IkoAfianando <ikoafianando123@gmail.com>
2021-12-25 09:19:21 +0700

    Push

--- readline.mjs
+++ readline.mjs
@@ -4,9 +4,12 @@ import readline from "readline";
 const input = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
 input.question("Siapakah nama anda?",(name) => {
     console.log(`Hello ${name}`);
-    input.close();
-})
+    input.question("Apa Hobi anda?", (hobbies) => {
+        console.log(`My Hobbies ${hobbies}`);
+        input.close();
+    });
+});
