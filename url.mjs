36f2affddad68328c6d9dc0ef90efeb7c2a22159
IkoAfianando <ikoafianando123@gmail.com>
2021-12-25 11:02:05 +0700

    Push

---
+++ url.mjs
@@ -0,0 +1,14 @@
+import {URL} from "url";
+
+const pzn = new URL("https://www.programmerzamannow.com/belajar?kelas=nodejs");
+
+// manipulasi url
+pzn.host = "www.khannedy.com";
+pzn.searchParams.append("status", "premium");
+
+console.info(pzn.toString());
+console.info(pzn.href);
+console.info(pzn.protocol);
+console.info(pzn.host);
+console.info(pzn.pathname);
+console.info(pzn.searchParams);
